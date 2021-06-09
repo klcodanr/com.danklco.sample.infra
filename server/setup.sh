dfbef48a6cebece7b3e6c81a0a57fdcbc897127e
Dan Klco <klco@adobe.com>
2021-06-09 14:55:04 -0400

    Improving the setup script

--- server/setup.sh
+++ server/setup.sh
@@ -3,22 +3,24 @@
 OWNER=klcodanr
 ARTIFACT_ID=com.danklco.sample.infra
 
+# Update 
+apt-get update
+
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh 
 apt-get install -y uidmap docker-compose
-dockerd-rootless-setuptool.sh install
 
 # Configure Docker
-loginctl enable-linger ubuntu
-service docker start
 usermod -aG docker ubuntu
+service docker start
 
 # Create SSH Key
 cat /dev/zero | ssh-keygen -q -N ""
 echo ""
 echo "Add public key to GitHub:"
 cat ~/.ssh/id_rsa.pub
-read -rsp $'Press any key to continue...\n' -n1 key
+read -rp $'Press any key to continue...\n' -n1 key
 
 # Clone the server configuration
 git clone git@github.com:${OWNER}/${ARTIFACT_ID}.git /opt/${ARTIFACT_ID}
@@ -32,6 +34,6 @@ mkdir -p /opt/${ARTIFACT_ID}/compose/work/logs/web
 mkdir -p /opt/${ARTIFACT_ID}/compose/work/secrets
 
 # Setup Docker start script
-cp /opt/${ARTIFACT_ID}/dockhostctr /etc/init.d
-update-rc.d dockhostctl defaults
+ln -s /opt/${ARTIFACT_ID}/server/docker-compose-ctl /etc/init.d/docker-compose-ctl
+update-rc.d docker-compose-ctl defaults
 
